46e27f7fa07b181ecd2d147e680ed4ccfac49d8d
Martin Dennhardt <martin.dennhardt.ext@siemens-healthineers.com>
2023-04-03 07:06:23 +0000

    use vitest it.each instead of loop

--- src/lib/counter.test.ts
+++ src/lib/counter.test.ts
@@ -3,17 +3,15 @@ import { describe, it, expect } from 'vitest';
 import { counter, increment } from './counter';
 
 describe('increment', () => {
-	for (const [before, after] of [
+	it.each([
 		[-1, 0],
 		[0, 1],
 		[1337, 1338]
-	]) {
-		it('increments counter by 1', () => {
-			counter.set(before);
+	])('increments counter by 1', (before, after) => {
+		counter.set(before);
 
-			increment();
+		increment();
 
-			expect(get(counter)).toBe(after);
-		});
-	}
+		expect(get(counter)).toBe(after);
+	});
 });
