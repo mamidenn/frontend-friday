00122b700597a681828c61f036905e1529b74012
Dennhardt, Martin (SHS DI CT R&D REL DP INST) <martin.dennhardt@siemens-healthineers.com>
2023-04-21 12:38:04 +0200

    set up workshop

---
+++ src/lib/sort.ts
@@ -0,0 +1,28 @@
+function sortInternal<T>(
+	array: T[],
+	start: number,
+	end: number,
+	isSmaller: (a: T, b: T) => boolean
+): T[] {
+	if (end - start < 2) return array;
+
+	let pivot = start;
+	for (let idx = start + 1; idx < end; ++idx) {
+		if (!isSmaller(array[start], array[idx])) {
+			const prev = array[++pivot];
+			array[pivot] = array[idx];
+			array[idx] = prev;
+		}
+	}
+	const prev = array[pivot];
+	array[pivot] = array[start];
+	array[start] = prev;
+
+	sortInternal(array, start, pivot, isSmaller);
+	sortInternal(array, pivot + 1, end, isSmaller);
+	return array;
+}
+
+export function sort<T>(array: T[]): T[] {
+	return sortInternal([...array], 0, array.length, (a, b) => a < b);
+}
