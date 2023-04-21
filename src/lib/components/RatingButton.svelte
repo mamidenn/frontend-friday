2f06a8abd8d088ddf469d11db7a9b3e14f97db5d
Dennhardt, Martin (SHS DI CT R&D REL DP INST) <martin.dennhardt@siemens-healthineers.com>
2023-04-21 11:19:56 +0200

    my solution for c2

---
+++ src/lib/components/RatingButton.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	export let selected = false;
+	let transition = false;
+	$: setTimeout(() => (transition = selected), 0);
+</script>
+
+<button
+	on:click
+	type="button"
+	class:selected
+	class="relative aspect-square w-12 rounded-full border border-black/10 font-medium transition-all hover:bg-orange-400"
+>
+	<slot />
+	{#if selected}
+		<div
+			class:expanded={transition}
+			class="absolute inset-0 -z-10 rounded-full transition-transform duration-300 ease-linear"
+		/>
+		<div
+			class:expanded2={transition}
+			class="transition-color absolute inset-0 -z-10 rounded-full delay-150 duration-500 ease-linear"
+		/>
+	{/if}
+</button>
+
+<style lang="postcss">
+	.selected {
+		@apply bg-yellow-400;
+	}
+	.expanded {
+		@apply scale-[20];
+		@apply bg-amber-100;
+	}
+	.expanded2 {
+		@apply scale-[20];
+		@apply bg-white;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,40 @@
 <script lang="ts">
-	import { counter, increment } from '../lib/counter';
+	import RatingButton from '$lib/components/RatingButton.svelte';
+	let rating: number;
 </script>
 
-<h1 class="text-4xl font-bold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button class="rounded-md border-2 border-black px-4 font-medium shadow-md" on:click={increment}>
-	Click me!
-</button>
-<p>Button clicked <span class="font-medium">{$counter}</span> times.</p>
+<div class="flex h-screen items-center justify-center bg-gray-50">
+	<div
+		class="relative isolate flex w-min flex-col overflow-hidden rounded-3xl border border-black/5 p-4 shadow-2xl"
+	>
+		<svg
+			viewBox="-81 -81 162 162"
+			class="h-12 w-12 rounded-full border bg-black fill-yellow-500 p-2"
+		>
+			<polygon points="0,-81 48,66 -77,-25 77,-25 -48,66" />
+		</svg>
+		<h2 class="mb-4 mt-2 text-2xl font-bold">How do you like Frontend Friday?</h2>
+		<p class="font-light">
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur amet ratione
+			reiciendis, neque culpa repudiandae repellendus possimus velit laudantium, placeat omnis rem.
+			Quam tempore quas, porro aliquam non est dolor?
+		</p>
+		<div class="my-4 flex flex-row gap-2">
+			{#each [1, 2, 3, 4, 5] as number}
+				<RatingButton on:click={() => (rating = number)} selected={rating === number}>
+					{number}
+				</RatingButton>
+			{/each}
+		</div>
+		<button
+			on:click={() => console.log('You submitted ' + rating + '.')}
+			type="submit"
+			class=" h-12 w-full rounded-3xl border border-black/10 bg-orange-400 font-medium"
+		>
+			Submit
+		</button>
+	</div>
+</div>
 
-<style>
+<style lang="postcss">
 </style>
