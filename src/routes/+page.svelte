a01cdc4b9923f2f851ae9f4161bd6a2c17bc6a3c
Stephan <stephan.mohr.ext@siemens-healthineers.com>
2023-06-16 13:54:03 +0200

    Step 4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,19 +6,19 @@
 		<div class="flex flex-col">
 			<img alt="hero" src="/illustration-hero.svg" />
 		</div>
-		<div class="flex flex-col justify-center text-center">
+		<div class="flex flex-col justify-center text-center gap-5">
 			<h1>Order Summary</h1>
 			<p>
 				You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you
 				like!
 			</p>
-			<div class="flex">
+			<div class="flex bg-neutral-very-pale-blue rounded-md gap-5 p-5">
 				<img alt="icon" src="/icon-music.svg" />
 				<div>
 					<h2>Annual Plan</h2>
 					<h3>$59.59/year</h3>
 				</div>
-				<a href="www.google.de">Change</a>
+				<a href="www.google.de" class="ml-auto">Change</a>
 			</div>
 			<button class="block rounded-md bg-primary-bright-blue text-neutral-very-pale-blue"
 				>Proceed to Payment</button
