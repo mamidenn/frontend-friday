6431b871c09acff0963c206995e6d3cb03c14876
Martin Dennhardt (SHS DI CT R&D REL DP INST) <martin.dennhardt@siemens-healthineers.com>
2023-06-16 12:22:11 +0000

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,28 +2,26 @@
 </script>
 
 <div class="grid min-h-screen place-items-center">
-	<div>
-		<div class="flex flex-col">
-			<img alt="hero" src="/illustration-hero.svg" />
-		</div>
-		<div class="flex flex-col justify-center text-center gap-5">
+	<div class="w-[450px]">
+		<img alt="hero" src="/illustration-hero.svg" />
+		<div class="flex flex-col gap-5 text-center">
 			<h1>Order Summary</h1>
 			<p>
 				You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you
 				like!
 			</p>
-			<div class="flex bg-neutral-very-pale-blue rounded-md gap-5 p-5">
+			<div class="flex items-center gap-5 rounded-md bg-neutral-very-pale-blue p-5">
 				<img alt="icon" src="/icon-music.svg" />
 				<div>
-					<h2>Annual Plan</h2>
-					<h3>$59.59/year</h3>
+					<div>Annual Plan</div>
+					<div>$59.59/year</div>
 				</div>
 				<a href="www.google.de" class="ml-auto">Change</a>
 			</div>
-			<button class="block rounded-md bg-primary-bright-blue text-neutral-very-pale-blue"
-				>Proceed to Payment</button
-			>
-			<button class="block">Cancel Order</button>
+			<button class="rounded-md bg-primary-bright-blue text-neutral-very-pale-blue">
+				Proceed to Payment
+			</button>
+			<button>Cancel Order</button>
 		</div>
 	</div>
 </div>
