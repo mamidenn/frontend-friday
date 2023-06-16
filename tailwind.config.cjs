cf622d6d372de467154e761facf05ebdbc3f6a3e
Martin Dennhardt <martin@dennhardt.de>
2023-06-16 10:28:52 +0000

    map style guide to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,29 @@
+/** @type {import("tailwindcss").Config} */
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {},
+		// presets from design/style-guide.md
+		colors: {
+			primary: {
+				'pale-blue': 'hsl(225 100% 94%)',
+				'bright-blue': 'hsl(245 75% 52%)'
+			},
+			neutral: {
+				'very-pale-blue': 'hsl(225 100% 98%)',
+				'desaturated-blue': 'hsl(224 23% 55%)',
+				'dark-blue': 'hsl(223 47% 23%)'
+			}
+		},
+		fontFamily: {
+			"sans": ['"Red Hat Display"', "sans-serif"]
+		},		
+		fontWeight: {
+			500: 500,
+			700: 700,
+			900: 900
+		}
 	},
 
 	plugins: []
