06b7868f65845609177bf4c6278e3163b4896e92
Stephan <stephan.mohr.ext@siemens-healthineers.com>
2023-06-16 13:19:50 +0200

    First step

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,18 @@
 </script>
 
 <div class="grid min-h-screen place-items-center">
-	<p>Your stuff here 🎯</p>
+	<div>
+		<div>
+			<img alt="hero" src="/illustration-hero.svg" />
+		</div>
+		<div>
+			<h1>Order Summary</h1>
+			<p>
+				You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you
+				like!
+			</p>
+		</div>
+	</div>
 </div>
 
 <style lang="postcss">
