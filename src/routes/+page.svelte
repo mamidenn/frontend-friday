73eeb8fe8a9e03628a4b96679c23594ed9959706
Philipp <philipp.zander.ext@siemens-healthineers.com>
2023-06-23 13:27:40 +0200

    button shadow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	<div class="w-[450px] overflow-hidden rounded-2xl bg-white">
 		<img alt="hero" src="/illustration-hero.svg" />
 		<div class="flex flex-col gap-5 p-10 text-center">
-			<h1>Order Summary</h1>
+			<h1 class="font-900 text-2xl">Order Summary</h1>
 			<p>
 				You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you
 				like!
@@ -18,7 +18,7 @@
 				</div>
 				<a href="www.google.de" class="ml-auto">Change</a>
 			</div>
-			<button class="rounded-md bg-primary-bright-blue text-neutral-very-pale-blue">
+			<button class="rounded-md bg-primary-bright-blue text-neutral-very-pale-blue py-3 shadow-xl shadow-primary-bright-blue/20">
 				Proceed to Payment
 			</button>
 			<button>Cancel Order</button>
