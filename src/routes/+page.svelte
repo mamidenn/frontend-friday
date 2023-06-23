c9d8ff3d3cbb5662d6bbbda2f66b09e1f01d57ab
Stefanie Voelker <stefanie.voelker.ext@siemens-healthineers.com>
2023-06-23 14:01:48 +0200

    commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,12 @@
 <div
 	class="bg:cover grid min-h-screen place-items-center bg-primary-pale-blue bg-[url('/pattern-background-desktop.svg')] bg-contain bg-no-repeat"
 >
-	<div class="w-[450px] overflow-hidden rounded-2xl bg-white shadow-lg">
+	<div
+		class="w-[450px] overflow-hidden rounded-2xl bg-white shadow-2xl shadow-primary-bright-blue/20"
+	>
 		<img alt="hero" src="/illustration-hero.svg" />
 		<div class="flex flex-col gap-9 p-10 text-center">
-			<h1 class="text-2xl font-900">Order Summary</h1>
+			<h1 class="text-2xl font-900 text-neutral-dark-blue">Order Summary</h1>
 			<p class="px-3 text-neutral-desaturated-blue">
 				You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you
 				like!
@@ -15,13 +17,13 @@
 			<div class="flex items-center gap-5 rounded-md bg-neutral-very-pale-blue p-5">
 				<img alt="icon" src="/icon-music.svg" />
 				<div>
-					<div class="font-900">Annual Plan</div>
+					<div class="font-900 text-neutral-dark-blue">Annual Plan</div>
 					<div class="text-neutral-desaturated-blue">$59.99/year</div>
 				</div>
 				<a href="www.google.de" class="ml-auto font-700 text-primary-bright-blue">Change</a>
 			</div>
 			<button
-				class="rounded-md bg-primary-bright-blue py-3 text-neutral-very-pale-blue shadow-xl shadow-primary-bright-blue/20"
+				class="rounded-md bg-primary-bright-blue py-3 font-700 text-neutral-very-pale-blue shadow-xl shadow-primary-bright-blue/20"
 			>
 				Proceed to Payment
 			</button>
