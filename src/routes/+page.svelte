e2004079792c817b68b1402c46c4f91153223a9b
Guenter <guenter.ott.ext@siemens-healthineers.com>
2023-06-23 13:34:11 +0200

    nl

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,20 +5,22 @@
 	<div class="w-[450px] overflow-hidden rounded-2xl bg-white">
 		<img alt="hero" src="/illustration-hero.svg" />
 		<div class="flex flex-col gap-5 p-10 text-center">
-			<h1 class="font-900 text-2xl">Order Summary</h1>
+			<h1 class="text-2xl font-900">Order Summary</h1>
 			<p>
 				You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you
 				like!
 			</p>
-			<div class="flex items-center gap-5 rounded-md bg-neutral-very-pale-blue p-5">
+			<div class="flex items-center gap-5 rounded-md bg-neutral-desaturated-blue p-5">
 				<img alt="icon" src="/icon-music.svg" />
 				<div>
-					<div>Annual Plan</div>
-					<div>$59.59/year</div>
+					<div class="font-900">Annual Plan</div>
+					<div class="text-neutral-desaturated-blue">$59.59/year</div>
 				</div>
-				<a href="www.google.de" class="ml-auto">Change</a>
+				<a href="www.google.de" class="ml-auto font-700 text-primary-bright-blue">Change</a>
 			</div>
-			<button class="rounded-md bg-primary-bright-blue text-neutral-very-pale-blue py-3 shadow-xl shadow-primary-bright-blue/20">
+			<button
+				class="rounded-md bg-primary-bright-blue py-3 text-neutral-very-pale-blue shadow-xl shadow-primary-bright-blue/20"
+			>
 				Proceed to Payment
 			</button>
 			<button>Cancel Order</button>
