a77510e0deade84096b8a51123c2fe7e8fa37b26
Stefanie Voelker <stefanie.voelker.ext@siemens-healthineers.com>
2023-07-07 12:30:00 +0200

    flags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,16 @@
 <script lang="ts">
-	import { json } from '@sveltejs/kit';
-	import { counter, increment } from '../lib/counter';
-
-	let name;
-	async function getData() {
-		name = fetch('https://restcountries.com/v3.1/all?fields=name');
+	function getData()
+	{
+		return fetch('https://restcountries.com/v3.1/all?fields=name,flags').then( (result) => result.json())
 	}
 </script>
 
-{#await fetch('https://restcountries.com/v3.1/all?fields=name').then( (result) => result.json() ) then data}
+{#await getData() then data}
 	<ul class="list-inside list-disc border-2">
 		{#each data as country}
-			<li>{country.name.common} : official {country.name.official}</li>
+			<li>{country.name.common} : official {country.name.official} <img src={country.flags.png} alt=""/></li>
 		{/each}
 	</ul>
-
-	<p>{data[0]}</p>
-	<p>{JSON.stringify(data)}</p>
 {/await}
 
 <style>
