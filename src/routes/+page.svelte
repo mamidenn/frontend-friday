09087e4bad3bd9a4107636ff1d3f9fc973629fa8
Stefanie Voelker <stefanie.voelker.ext@siemens-healthineers.com>
2023-07-07 12:00:37 +0200

    commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,16 @@
 <script lang="ts">
 	import { counter, increment } from '../lib/counter';
 
-	let name = fetch("https://restcountries.com/v3.1/all?fields=name")
-
-	name.
+	let name;
+	async function getData(){
+		name = fetch("https://restcountries.com/v3.1/all?fields=name")
+	}
 </script>
 
-<h1 class="text-4xl font-bold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button class="rounded-md border-2 border-black px-4 font-medium shadow-md" on:click={increment}>
-	Click me!
-</button>
-<p>Button clicked <span class="font-medium">{$counter}</span> times.</p>
-
-{#await}
+{#await fetch("https://restcountries.com/v3.1/all?fields=name")}
+{:then data}
+<p>{data}</p>
+{:catch}
 {/await}
 
 <style>
