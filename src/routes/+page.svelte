adba920ce61fcebb99a9c47fe7333c8c22a88cca
Marco Heilmann <marcoheilmann@gmx.net>
2023-07-07 12:23:57 +0200

    asdf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,20 @@
 	import { counter, increment } from '../lib/counter';
 
 	let name;
-	async function getData(){
-		name = fetch("https://restcountries.com/v3.1/all?fields=name")
+	async function getData() {
+		name = fetch('https://restcountries.com/v3.1/all?fields=name');
 	}
 </script>
 
-{#await  fetch("https://restcountries.com/v3.1/all?fields=name").then(result => result.json())}
-{:then data}
-<p>{data[0]}</p>
-<p>{JSON.stringify(data)}</p>
-{:catch}
+{#await fetch('https://restcountries.com/v3.1/all?fields=name').then( (result) => result.json() ) then data}
+	<ul class="list-inside list-disc border-2">
+		{#each data as country}
+			<li>{country.name.common} : official {country.name.official}</li>
+		{/each}
+	</ul>
+
+	<p>{data[0]}</p>
+	<p>{JSON.stringify(data)}</p>
 {/await}
 
 <style>
