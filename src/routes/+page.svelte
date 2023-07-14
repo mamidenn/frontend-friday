461586705766bde34756633396b4ded539367e9c
Matthias <matthias.jentsch@siemens-healthineers.com>
2023-07-14 15:14:16 +0200

    Nice layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,17 +18,40 @@
 	$: countries = getData(filter)
 </script>
 
-<div>
-	<input bind:value={filter} />
+<div class="justify-center p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 my-7">
+	<input 
+		class="text-center text-2xl border-none outline-none"
+		placeholder="Search..." 
+		bind:value={filter} 
+	/>
+</div>
+<div class="p-6 max-w-3xl mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 my-7">
+	<div>
+		<div>
+			{#await countries then data}
+				<ul class="list-inside">
+					{#each data as country}
+						<li class="font-medium py-6 text-center">
+							<div class="py-2">
+								<p class="text-2xl">{country.name.common}</p>
+								<p class="text-m">({country.name.official})</p>
+							</div>
+							<div class="py-2">
+								<img class="px-56" src={country.flags.png} alt=""/>
+							</div>
+							
+						</li>
+					{/each}
+				</ul>
+			{/await}
+		</div>
+	</div>
 </div>
 
-{#await countries then data}
-	<ul class="list-inside list-disc border-2">
-		{#each data as country}
-			<li>{country.name.common} : official {country.name.official} <img src={country.flags.png} alt=""/></li>
-		{/each}
-	</ul>
-{/await}
+
 
 <style>
+	:global(body) {
+        background-color: #e5e7eb;
+    }
 </style>
