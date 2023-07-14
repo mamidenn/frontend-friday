6350415b342df203759eaf13503e324a979b289a
Matthias <matthias.jentsch@siemens-healthineers.com>
2023-07-14 14:02:32 +0200

    Implement filter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
-	function getData()
-	{
-		return fetch('https://restcountries.com/v3.1/all?fields=name,flags').then( (result) => result.json())
+	interface Country {
+		name: {
+			common: string,
+			official: string
+		},
+		flags: {
+			png: string
+		}
 	}
+
+	async function getData(filter: string)
+	{
+		let countries : Country[] = await fetch('https://restcountries.com/v3.1/all?fields=name,flags').then( (result) => result.json());
+		return countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()));
+	}
+	let filter = "";
+	$: countries = getData(filter)
 </script>
 
-{#await getData() then data}
+<div>
+	<input bind:value={filter} />
+</div>
+
+{#await countries then data}
 	<ul class="list-inside list-disc border-2">
 		{#each data as country}
 			<li>{country.name.common} : official {country.name.official} <img src={country.flags.png} alt=""/></li>
