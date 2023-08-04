bb18777d49c70848022880d84d7576a7ade94cbd
Guenter <guenter.ott.ext@siemens-healthineers.com>
2023-08-04 13:31:13 +0200

    goingon

--- src/routes/[country]/+page.ts
+++ src/routes/[country]/+page.ts
@@ -1,5 +1,7 @@
-import type { PageLoad } from './[country]/$types';
+import type { PageLoad } from './$types';
 
-export const load = (async ({ params }) => {
+export const load = (async ({ params }) => 
+{
+	let allCountries : Promise<Country[]> = fetch('https://restcountries.com/v3.1/alpha/'+ params.country).then( (result) => result.json());
 	return { country: params.country };
 }) satisfies PageLoad;
