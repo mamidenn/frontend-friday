e348e6414ba0de7cc1e9593867a1d047cb19c03a
Matthias <matthias.jentsch@siemens-healthineers.com>
2023-08-04 13:37:30 +0200

    Pass country object to detials page

--- src/routes/[country]/+page.svelte
+++ src/routes/[country]/+page.svelte
@@ -3,4 +3,16 @@
 	export let data: PageData;
 </script>
 
-Hi from {data.country}
+Hi from {data.cca3}
+
+{JSON.stringify(data.country)}
+
+
+<!--
+	TODO: Official Name
+		  Common Name
+		  Region
+		  Languages in english
+		  Capital
+		  Neighbours
+-->
--- src/routes/[country]/+page.ts
+++ src/routes/[country]/+page.ts
@@ -2,6 +2,9 @@ import type { PageLoad } from './$types';
 
 export const load = (async ({ params }) => 
 {
-	let allCountries : Promise<Country[]> = fetch('https://restcountries.com/v3.1/alpha/'+ params.country).then( (result) => result.json());
-	return { country: params.country };
+	const allCountries = fetch('https://restcountries.com/v3.1/alpha/'+ params.country).then( (result) => result.json());
+	return { 
+		cca3: params.country,
+		country: allCountries
+	};
 }) satisfies PageLoad;
