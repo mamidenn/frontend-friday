a027d7a2495247f4127e8f7cdc908c26041f4810
Marco Heilmann <marcoheilmann@gmx.net>
2023-08-11 13:13:44 +0200

    asdf

--- src/lib/country.ts
+++ src/lib/country.ts
@@ -3,10 +3,20 @@ export interface Country {
     name: {
         common: string;
         official: string;
+        nativeName: Record <string, Record<string, string>>;
     };
+    tld: string[];
+    cca2: string;
+    ccn3: string;
+    cca3: string;
+    cioc: string;
+    independent: boolean;
+    status: string;
+    unMember: boolean;
+    currencies: 
     flags: {
         png: string;
     };
     capital: {};
-    cca3: string;
+
 }
