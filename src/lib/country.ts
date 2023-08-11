26567fdfd149cb6ecf0c46380f9b47057e112543
Philipp <philipp.zander.ext@siemens-healthineers.com>
2023-08-11 13:21:19 +0200

    asdf

--- src/lib/country.ts
+++ src/lib/country.ts
@@ -1,22 +1,26 @@
-
 export interface Country {
-    name: {
-        common: string;
-        official: string;
-        nativeName: Record <string, Record<string, string>>;
-    };
-    tld: string[];
-    cca2: string;
-    ccn3: string;
-    cca3: string;
-    cioc: string;
-    independent: boolean;
-    status: string;
-    unMember: boolean;
-    currencies: 
-    flags: {
-        png: string;
-    };
-    capital: {};
-
+	name: {
+		common: string;
+		official: string;
+		nativeName: Record<string, { offcial: string; common: string }>;
+	};
+	tld: string[];
+	cca2: string;
+	ccn3: string;
+	cca3: string;
+	cioc: string;
+	independent: boolean;
+	status: string;
+	unMember: boolean;
+	currencies: Record<string, { name: string; symbol: string }>;
+	idd: { root: string; suffixes: string[] };
+	capital: string[];
+	altSpellings: string[];
+    region:string;
+    subregion: string;
+    languages: Record<string, string>;
+    
+	flags: {
+		png: string;
+	};
 }
