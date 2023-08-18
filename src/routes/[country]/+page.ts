15bfb996d433e41faa244d831e0f2ef45415f87a
Stephan <stephan.mohr.ext@siemens-healthineers.com>
2023-08-18 13:44:41 +0200

    Fix fetch

--- src/routes/[country]/+page.ts
+++ src/routes/[country]/+page.ts
@@ -1,7 +1,7 @@
 import type { PageLoad } from './$types';
 import type { Country } from '$lib/country';
 
-export const load = (async ({ params }) => {
+export const load = (async ({ fetch, params }) => {
 	const fetchedCountry = fetch('https://restcountries.com/v3.1/alpha/' + params.country).then(
 		(result) => result.json()
 	);
