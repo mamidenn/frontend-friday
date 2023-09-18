1559b5954202baec3c0c753cb1c00b83a5bf357c
Martin Dennhardt (SHS DI CT R&D REL DP INST) <martin.dennhardt@siemens-healthineers.com>
2023-09-18 11:15:54 +0200

    enable passing custom fetcher to getUser

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 /**
  * # Including/Excluding fields
  *
@@ -30,9 +28,18 @@ import fetch from 'node-fetch';
  * https://randomuser.me/api/?inc=gender,name,nat
  * ```
  */
-export const getUser = async function <T extends keyof User>(...fields: T[]) {
-	// please do not change the seed, the tests depend on it
-	const seed = import.meta.env.MODE === "test" ? "frontendfriday" : ""
+export const getUser = async function <T extends keyof User>(
+	fieldOrFetch?: T | typeof globalThis.fetch,
+	...fields: T[]
+) {
+	fields =
+		fieldOrFetch !== undefined && typeof fieldOrFetch !== 'function'
+			? [fieldOrFetch, ...fields]
+			: fields;
+	const seed = import.meta.env.MODE === 'test' ? 'frontendfriday' : '';
+	const fetch =
+		typeof fieldOrFetch === 'function' ? fieldOrFetch : (await import('node-fetch')).default;
+
 	const response = await fetch(`https://randomuser.me/api/?seed=${seed}&inc=${fields}`);
 	const json = (await response.json()) as UserApiResult;
 	return json.results[0] as Pick<User, T>;
