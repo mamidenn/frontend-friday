a4f97346bb1404ac4d7bc3bc76b2638ef54488ba
Marco Heilmann <marcoheilmann@gmx.net>
2023-09-22 13:30:20 +0200

    asdf

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -11,55 +11,54 @@
 	<title>User</title>
 </svelte:head>
 
+<div class="m-10 justify-center rounded-xl bg-slate-500 p-5 text-white">
+	<h2 class="">{user.name.title} {user.name.first} {user.name.last}</h2>
+	<div class="flex flex-row">
+		<div class="">
+			<img
+				src={user.picture.large}
+				alt="A picture of {user.name.title} {user.name.first} {user.name.last}"
+			/>
+		</div>
 
-<div class="bg-slate-500 rounded-xl p-5 m-10 flex flex-col justify-center text-white">
-	<h2>{user.name.title} {user.name.first} {user.name.last}</h2>
-	<div>
-		<img
-			src={user.picture.large}
-			alt="A picture of {user.name.title} {user.name.first} {user.name.last}"
-		/>
+		<div class="flex flex-col">
+			<table>
+				<tr>
+					<th>Location</th>
+					<td>{user.location.city}, {user.location.state}, {user.location.country}</td>
+				</tr>
+				<tr>
+					<th>Email</th>
+					<td>{user.email}</td>
+				</tr>
+				<tr>
+					<th>Phone</th>
+					<td>{user.phone}</td>
+				</tr>
+				<tr>
+					<th>Mobile</th>
+					<td>{user.cell}</td>
+				</tr>
+				<tr>
+					<th>Date Of Birth</th>
+					<td>{user.dob.date.toDateString()} ({user.dob.age})</td>
+				</tr>
+			</table>
+		</div>
+
+		<div class="h-96 w-80">
+			<OpenStreetMap
+				{coordinates}
+				markerStyle={{
+					'circle-radius': 10,
+					'circle-stroke-color': [255, 0, 127],
+					'circle-stroke-width': 2
+				}}
+				zoom={12}
+			/>
+		</div>
 	</div>
-
-<div>
-	<table>
-		<tr>
-			<th>Location</th>
-			<td>{user.location.city}, {user.location.state}, {user.location.country}</td>
-		</tr>
-		<tr>
-			<th>Email</th>
-			<td>{user.email}</td>
-		</tr>
-		<tr>
-			<th>Phone</th>
-			<td>{user.phone}</td>
-		</tr>
-		<tr>
-			<th>Mobile</th>
-			<td>{user.cell}</td>
-		</tr>
-		<tr>
-			<th>Date Of Birth</th>
-			<td>{user.dob.date.toDateString()} ({user.dob.age})</td>
-		</tr>
-	</table>
 </div>
-	
-
-	<div class="h-96">
-		<OpenStreetMap
-			{coordinates}
-			markerStyle={{
-				'circle-radius': 10,
-				'circle-stroke-color': [255, 0, 127],
-				'circle-stroke-width': 2
-			}}
-			zoom={12}
-		/>
-	</div>
-</div>
-
 
 <style lang="postcss">
 </style>
