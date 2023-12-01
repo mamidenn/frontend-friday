a3c86a65ef58c09e021d31b04cc9174ef5ff6918
Christian Stadter <christian.stadter.ext@siemens-healthineers.com>
2023-12-01 13:26:48 +0100

    nix

--- src/lib/DarkModeToggle.svelte
+++ src/lib/DarkModeToggle.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { browser } from "$app/environment";
+
 	// 1. use data binding to set class "dark" on the body element when the
 	//    checkbox is checked
 	// 2. create a "toggle switch" ui something like this:
@@ -7,6 +9,11 @@
 	// 4. hide the checkbox
 
 	let darkMode = false;
+
+	if(browser) {
+		const bodyElement=document.getElementById("body");
+		console.log(bodyElement);
+	}
 </script>
 
 <label>
