d7695a548c2764434512eaac320b03c648e95895
Dennhardt, Martin (SHS DI CT R&D REL DP INST) <martin.dennhardt@siemens-healthineers.com>
2023-12-01 12:58:53 +0100

    add dark mode challenge

---
+++ src/lib/DarkModeToggle.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	// 1. use data binding to set class "dark" on the body element when the
+	//    checkbox is checked
+	// 2. create a "toggle switch" ui something like this:
+	//    [()  ]   <=>   [  ()]
+	// 3. use interaction with the toggle switch to control the checkbox
+	// 4. hide the checkbox
+</script>
+
+<label>
+	<input type="checkbox" />
+	Dark mode
+</label>
